cf3286c32aa66fe564eb97196dae25a17bdaae9e
zhangqi61 <zhangqi61@baidu.com>
2022-01-22 22:53:11 +0800

    [update]

---
+++ app/src/main/java/com/dashingqi/dqkotlin/type/TypeClass.kt
@@ -0,0 +1,27 @@
+package com.dashingqi.dqkotlin.type
+
+/**
+ * @desc :
+ * @author : zhangqi
+ * @time : 2022/1/22 18:55
+ */
+// kotlin 代码
+
+// 用 val 定义可为空、不可为空的Long，并且赋值
+val a: Long = 1L
+val b: Long? = 2L
+
+// 用 var 定义可为空、不可为空的Long，并且赋值
+var c: Long = 3L
+var d: Long? = 4L
+
+// 用 var 定义可为空的Long，先赋值，然后改为null
+var e: Long? = 5L
+e = null
+
+// 用 val 定义可为空的Long，直接赋值null
+val f: Long? = null
+
+// 用 var 定义可为空的Long，先赋值null，然后赋值数字
+var g: Long? = null
+g = 6L
