abe02cb46a803464c420cbfd11542c9da17242ea
zhangqi61 <zhangqi61@baidu.com>
2022-01-22 18:51:29 +0800

    [update]

--- app/src/main/java/com/dashingqi/dqkotlin/MainActivity.kt
+++ app/src/main/java/com/dashingqi/dqkotlin/MainActivity.kt
@@ -2,10 +2,8 @@ package com.dashingqi.dqkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.dashingqi.dqkotlin.extend.Person
 import kotlin.concurrent.thread
-import kotlin.system.exitProcess
+
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
