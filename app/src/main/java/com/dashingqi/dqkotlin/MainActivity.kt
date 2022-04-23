8bc71ef2bbe7a3dad9776f42cf3bd11ddafacac4
zhangqi61 <zhangqi61@baidu.com>
2022-04-23 10:25:39 +0800

    [update]

--- app/src/main/java/com/dashingqi/dqkotlin/MainActivity.kt
+++ app/src/main/java/com/dashingqi/dqkotlin/MainActivity.kt
@@ -2,7 +2,10 @@ package com.dashingqi.dqkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.dashingqi.coroutine.current.coroutineCurrent
 import com.dashingqi.coroutine.current.coroutineCurrentJobs
 import com.dashingqi.coroutine.follow.launchIn
@@ -14,6 +17,7 @@ import com.dashingqi.coroutine.select.selectChannelMethod
 import com.dashingqi.coroutine.select.selectMethod
 import com.dashingqi.http.HttpV2
 import com.dashingqi.service.ApiService
+import com.hjq.xtoast.XToast
 import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
@@ -23,6 +27,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val runnable = Runnable {
+            XToast<XToast<*>>(this)
+                .apply {
+                    setContentView(R.layout.layout_toast_hint)
+                    setText(R.id.message, "点我消失")
+                    setDuration(1000)
+                    setOnClickListener(
+                        R.id.message,
+                        XToast.OnClickListener<TextView?> { toast: XToast<*>, view: TextView? ->
+                            toast.cancel()
+
+                        })
+
+                }
+                .show()
+        }
+        findViewById<TextView>(R.id.hl).postDelayed(runnable,6000)
         thread {
             Runnable {
                 Log.d(TAG, "current thread is ${Thread.currentThread().name} ")
@@ -54,6 +76,33 @@ class MainActivity : AppCompatActivity() {
         result.onSuccess {
             Log.d(TAG, "onSuccess perform result is $it")
         }
+
+        AlertDialog
+            .Builder(this)
+            .setTitle("测试测试")
+            .setMessage("这是一个严肃的测试")
+            .setCancelable(false)
+            .setPositiveButton("弹出Toast") { dialog, which ->
+
+            }
+            .setNegativeButton("弹出Toast") { dialog, which ->
+                XToast<XToast<*>>(this)
+                    .apply {
+                        setContentView(R.layout.layout_toast_hint)
+                        setText(R.id.message, "点我消失")
+                        setDuration(1000)
+                        setOnClickListener(
+                            R.id.message,
+                            XToast.OnClickListener<TextView?> { toast: XToast<*>, view: TextView? ->
+                                toast.cancel()
+
+                            })
+
+                    }
+                    .show()
+            }
+            .show()
+
     }
 
     companion object {
