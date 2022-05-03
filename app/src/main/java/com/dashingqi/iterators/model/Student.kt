d67d96921bd85c0ca8af95ac1e5aba6ebc21dc95
zhangqi61 <zhangqi61@baidu.com>
2022-05-03 16:52:05 +0800

    [集合操作符]

---
+++ app/src/main/java/com/dashingqi/iterators/model/Student.kt
@@ -0,0 +1,11 @@
+package com.dashingqi.iterators.model
+
+/**
+ * @desc : 学生数据类
+ * @author : zhangqi
+ * @time : 2022/5/3 15:26
+ */
+data class Student(
+    val name: String = "",
+    val score: Int = 0
+)
