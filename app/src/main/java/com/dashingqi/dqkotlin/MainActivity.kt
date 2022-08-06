da6e32591763d811eb65896521998bbec88fff67
zhangqi61 <zhangqi61@baidu.com>
2022-08-06 16:27:30 +0800

    [update]

--- app/src/main/java/com/dashingqi/dqkotlin/MainActivity.kt
+++ app/src/main/java/com/dashingqi/dqkotlin/MainActivity.kt
@@ -2,18 +2,7 @@ package com.dashingqi.dqkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
-import com.dashingqi.coroutine.exception.coroutineException
-import com.dashingqi.coroutine.current.*
-import com.dashingqi.coroutine.exception.structMain
-import com.dashingqi.coroutine.exception.supervisorJobMethod
-import com.dashingqi.http.HttpV2
-import com.dashingqi.service.ApiService
-import com.hjq.xtoast.XToast
-import kotlinx.coroutines.runBlocking
-import kotlin.concurrent.thread
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,96 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mapOperate()
-
-//        val runnable = Runnable {
-//            XToast<XToast<*>>(this)
-//                .apply {
-//                    setContentView(R.layout.layout_toast_hint)
-//                    setText(R.id.message, "点我消失")
-//                    setDuration(1000)
-//                    setOnClickListener(
-//                        R.id.message,
-//                        XToast.OnClickListener<TextView?> { toast: XToast<*>, view: TextView? ->
-//                            toast.cancel()
-//
-//                        })
-//
-//                }
-//                .show()
-//        }
-//        findViewById<TextView>(R.id.hl).postDelayed(runnable, 6000)
-//        thread {
-//            Runnable {
-//                Log.d(TAG, "current thread is ${Thread.currentThread().name} ")
-//
-//                val wanAndroidService = HttpV2.create<ApiService>()
-//                val wxArticle = wanAndroidService.repos(lang = "kotlin", since = "weekly")
-//                Log.d(TAG, "wxArticle data is ${wxArticle?.msg}")
-//            }.run()
-//        }
-
-        runBlocking {
-//            onFlowLifecycle()
-//            onFlowCatch()
-//            selectMethod()
-            //selectChannelMethod()
-//            selectChannelMethod()
-//            cancelAllDeferred()
-//            coroutineCurrent()
-            //coroutineCurrentJobs()
-//            coroutineMutexMain()
-//            actorMain()
-//            noShareState()
-//            noShareStateMethod()
-        }
-
-        val result = kotlin.runCatching {
-            val i = 3
-            6
-        }
-        result.onFailure {
-            Log.d(TAG, "onFailure perform  exception is $it")
-        }
-        result.onSuccess {
-            Log.d(TAG, "onSuccess perform result is $it")
-        }
-        findViewById<TextView>(R.id.hl).setOnClickListener {
-            AlertDialog
-                .Builder(this)
-                .setTitle("测试测试")
-                .setMessage("这是一个严肃的测试")
-                .setCancelable(false)
-                .setPositiveButton("弹出Toast") { dialog, which ->
-
-                }
-                .setNegativeButton("弹出Toast") { dialog, which ->
-                    XToast<XToast<*>>(this)
-                        .apply {
-                            setContentView(R.layout.layout_toast_hint)
-                            setText(R.id.message, "点我消失")
-                            setDuration(1000)
-                            setOnClickListener(
-                                R.id.message,
-                                XToast.OnClickListener<TextView?> { toast: XToast<*>, view: TextView? ->
-                                    toast.cancel()
-
-                                })
-
-                        }
-                        .show()
-                }
-                .show()
-        }
-
-        coroutineException()
-
-        supervisorJobMethod()
-
-    }
-
-    companion object {
-        private const val TAG = "MainActivity"
     }
 
 }
