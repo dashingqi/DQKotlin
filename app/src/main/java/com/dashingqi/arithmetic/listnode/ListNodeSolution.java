2937cc4bd808c6378e4beecf2c66b84f20127edf
zhangqi61 <zhangqi61@baidu.com>
2023-02-06 20:47:11 +0800

    【反转列表】

--- app/src/main/java/com/dashingqi/arithmetic/listnode/ListNodeSolution.java
+++ app/src/main/java/com/dashingqi/arithmetic/listnode/ListNodeSolution.java
@@ -42,4 +42,22 @@ class ListNodeSolution {
 
         return pre.next;
     }
+
+    /**
+     * 反转链表
+     *
+     * @param head
+     * @return 反转后的链表
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = null;
+        ListNode pre = head;
+        while (pre != null) {
+            ListNode tempNode = pre.next;
+            pre.next = cur;
+            cur = pre;
+            pre = tempNode;
+        }
+        return cur;
+    }
 }
