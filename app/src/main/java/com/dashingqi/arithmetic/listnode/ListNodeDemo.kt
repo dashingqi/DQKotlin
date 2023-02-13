a7a318715f8e1ee309bf1b3eef850f1083cdc3d8
zhangqi61 <zhangqi61@baidu.com>
2023-02-13 23:01:45 +0800

    【环形链表】

--- app/src/main/java/com/dashingqi/arithmetic/listnode/ListNodeDemo.kt
+++ app/src/main/java/com/dashingqi/arithmetic/listnode/ListNodeDemo.kt
@@ -24,6 +24,19 @@ class ListNodeDemo {
         return cur
     }
 
+    internal fun reverseListNode2(head: ListNode): ListNode? {
+        var cur: ListNode? = null
+        var pre: ListNode? = head
+        while (pre != null) {
+            val tempNode = pre.next
+            pre.next = cur
+            cur = pre
+            pre = tempNode
+        }
+
+        return cur
+    }
+
     /**
      * 环形链表 通过快慢指针
      * @param head ListNode?
@@ -43,4 +56,31 @@ class ListNodeDemo {
         }
         return false
     }
+
+    internal fun hasCycle1(head: ListNode?): Boolean {
+        head ?: return false
+        head.next ?: return false
+        var fast: ListNode? = head
+        var slow: ListNode? = head
+        while (fast?.next != null) {
+            fast = fast.next?.next
+            slow = slow?.next
+            if (fast == slow) {
+                return true
+            }
+        }
+        return false
+    }
+
+    /**
+     * 合并两个有序链表
+     * @param list1 ListNode?
+     * @param list2 ListNode?
+     * @return ListNode?
+     */
+//    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+//        list1 ?: return list2
+//        list2 ?: return list1
+//
+//    }
 }
