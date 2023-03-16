1fbb556c21d404a708d8fa653f086616a3304e17
zhangqi61 <zhangqi61@baidu.com>
2023-03-16 20:47:16 +0800

    【Update】【折半查找】变种实现

--- app/src/main/java/com/dashingqi/arithmetic/search/HalfSearch.java
+++ app/src/main/java/com/dashingqi/arithmetic/search/HalfSearch.java
@@ -25,11 +25,12 @@ public class HalfSearch {
 
     /**
      * 查找第一个值等于给定值的元素
-     * @param array
-     * @param low
-     * @param high
-     * @param value
-     * @return
+     *
+     * @param array 数据源
+     * @param low   左角标
+     * @param high  右角标
+     * @param value 目标值
+     * @return index
      */
     private static int bSearchFirstValueInternally(int[] array, int low, int high, int value) {
         while (low <= high) {
@@ -43,6 +44,85 @@ public class HalfSearch {
                     return mid;
                 } else {
                     high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找最后一个值等于给定值的元素
+     *
+     * @param array 数据源
+     * @param low   左角标
+     * @param high  右角标
+     * @param value 目标值
+     * @return index
+     */
+    private static int bSearchLastValueInternally(int[] array, int low, int high, int value) {
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (array[mid] > value) {
+                high = mid - 1;
+            } else if (array[mid] < value) {
+                low = mid + 1;
+            } else {
+                // 首先保证角标或者保证是最后一个值；
+                if (mid == array.length - 1 || array[mid + 1] != value) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     * 查找第一个值大于给定值的元素
+     *
+     * @param array 数据源
+     * @param low   左角标
+     * @param high  右角标
+     * @param value 目标值
+     * @return index
+     */
+    private static int bSearchFirstMaxValueInternally(int[] array, int low, int high, int value) {
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (array[mid] > value) {
+                if (mid == 0 || array[mid - 1] < value) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找最后一个小于给定值的元素
+     *
+     * @param array 数据源
+     * @param low   左角标
+     * @param high  右角标
+     * @param value 目标值
+     * @return index
+     */
+    private static int bSearchLastMinValueInternally(int[] array, int low, int high, int value) {
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (array[mid] > value) {
+                high = mid - 1;
+            } else {
+                if (mid == array.length - 1 || array[mid + 1] > value) {
+                    return mid;
+                } else {
+                    low = mid + 1;
                 }
             }
         }
